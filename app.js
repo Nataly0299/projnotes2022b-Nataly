89087a430df1a674340fcdbed73f70f3d193ea4e
Nataly0299 <brendarocha0299@gmail.com>
2022-09-22 22:30:21 -0500

    se agraga js aww

--- app.js
+++ app.js
@@ -1,23 +1,43 @@
+//Biblioteca de 3ros para manejar errores http 
 var createError = require('http-errors');
+//El fremework express
 var express = require('express');
+//Biblioteca del nucleo de node que sirve para administrar rutas
 var path = require('path');
+//Biblioteca externa que sirve para administrar cookis
 var cookieParser = require('cookie-parser');
+//Biblioteca que registra en consola solicitudes del cliente
 var logger = require('morgan');
 
+//Definición de rutas
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
+//Creando una instancia de express
 var app = express();
 
 // view engine setup
+//Configura el motor de plantillas 
+//1. Establecer donde estarán las plantillas
+//(Vistas ->Views)
+//app.set("<nombre de la var>",<valor>)
 app.set('views', path.join(__dirname, 'views'));
+//console.log(`El valor del join es: ${path.join(__dirname,'views')}`);
+//Establezco que motor precargado usare
 app.set('view engine', 'hbs');
 
+//Establezco Middleware
 app.use(logger('dev'));
+//Middleware para parsear a json la peticion 
 app.use(express.json());
+//Decodificar la url
 app.use(express.urlencoded({ extended: false }));
+//Parsear cookis
 app.use(cookieParser());
+//Servidor  de archivos estáticos 
 app.use(express.static(path.join(__dirname, 'public')));
+
+//render requiere de una plantilla y send envia datos crudos 
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
