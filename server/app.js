6a3b5abc769a593b7d1dcf2fe74d730a0ce910bc
Nataly0299 <brendarocha0299@gmail.com>
2022-10-09 21:35:13 -0500

    ES6

--- server/app.js
+++ server/app.js
@@ -1,34 +1,65 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+//Biblioteca de 3ros para manejar errores http 
+//var createError = require('http-errors');
+//ES6
+import createError from 'http-errors';
+//El fremework express
+//var express = require('express');
+import express from'express';
+//Biblioteca del nucleo de node que sirve para administrar rutas
+//var path = require('path');
+import path from 'path';
+//Biblioteca externa que sirve para administrar cookis
+//var cookieParser = require('cookie-parser');
+import cookieParser from 'cookie-parser';
+//Biblioteca que registra en consola solicitudes del cliente
+//var logger = require('morgan');
+import logger from'morgan';
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+//Definición de rutas
+//var indexRouter = require('./routes/index');
+//var usersRouter = require('./routes/users');
 
-var app = express();
+import indexRouter from './routes/index';
+import usersRouter from'./routes/users';
+//Creando una instancia de express
+//var app = express();
+const app = express();
 
 // view engine setup
+//Configura el motor de plantillas 
+//1. Establecer donde estarán las plantillas
+//(Vistas ->Views)
+//app.set("<nombre de la var>",<valor>)
 app.set('views', path.join(__dirname, 'views'));
+//console.log(`El valor del join es: ${path.join(__dirname,'views')}`);
+//Establezco que motor precargado usare
 app.set('view engine', 'hbs');
 
+//Establezco Middleware
 app.use(logger('dev'));
+//Middleware para parsear a json la peticion 
 app.use(express.json());
+//Decodificar la url
 app.use(express.urlencoded({ extended: false }));
+//Parsear cookis
 app.use(cookieParser());
+//Servidor  de archivos estáticos 
 app.use(express.static(path.join(__dirname, 'public')));
+
+//render requiere de una plantilla y send envia datos crudos 
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
+//app.use(function(req, res, next) {
+app.use((req, res, next) =>{
   next(createError(404));
 });
 
 // error handler
-app.use(function(err, req, res, next) {
+//app.use(function(err, req, res, next) {
+app.use((err, req, res, next)=> {
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
@@ -37,5 +68,8 @@ app.use(function(err, req, res, next) {
   res.status(err.status || 500);
   res.render('error');
 });
-
-module.exports = app;
+//Exportando la instancia del server "app"
+//ES5
+//module.exports = app;
+//ES6
+export default app;
---
+++ server/bin/www.js
@@ -0,0 +1,113 @@
+#!/usr/bin/env node
+
+/**
+ * Module dependencies.
+ */
+
+
+//ES5
+//var app = require('../app');
+//ES6
+import app from"../app"
+import debug from 'debug';
+import http from 'http';
+//var debug = require('debug')('projnotes-2022b:server');
+//var http = require('http');
+
+/**
+ * Get port from environment and store in Express.
+ */
+
+//var port = normalizePort(process.env.PORT || '3000');
+const port = normalizePort(process.env.PORT || '3000');
+app.set('port', port);
+
+/**
+ * Create HTTP server.
+ */
+//app es una funcion de tipo middleware(codigointermediario)
+//(req,res) =>{...res.send("algo")}
+//var server = http.createServer(app);
+const server = http.createServer(app);
+
+/**
+ * Listen on provided port, on all network interfaces.
+ */
+
+server.listen(port);
+server.on('error', onError);
+server.on('listening', onListening);
+
+/**
+ * Normalize a port into a number, string, or false.
+ */
+
+function normalizePort(val) {
+  var port = parseInt(val, 10);
+
+  if (isNaN(port)) {
+    // named pipe
+    return val;
+  }
+
+  if (port >= 0) {
+    // port number
+    return port;
+  }
+
+  return false;
+}
+
+/**
+ * Event listener for HTTP server "error" event.
+ */
+
+function onError(error) {
+  if (error.syscall !== 'listen') {
+    throw error;
+  }
+
+  var bind = typeof port === 'string'
+    //? 'Pipe ' + port  
+    ? `Pipe ${port}` 
+    //: 'Port ' + port;
+    : `Port ${port}`
+
+
+  // handle specific listen errors with friendly messages
+  switch (error.code) {
+    case 'EACCES':
+      //console.error(bind + ' requires elevated privileges');
+      console.error(`${bind} requires elevated privileges`);
+      process.exit(1);
+      break;
+    case 'EADDRINUSE':
+      //console.error( bind + ' is already in use');
+      console.error(`${bind} is already in use`);
+      process.exit(1);
+      break;
+    default:
+      throw error;
+  }
+}
+
+/**
+ * Event listener for HTTP server "listening" event.
+ */
+
+function onListening() {
+  
+  const addr = server.address();
+  const {port} = addr;
+  //var bind = typeof addr === 'string'
+  var bind = typeof addr === 'string' 
+   // ? 'pipe ' + addr
+    //: 'port ' + addr.port;
+    ? `pipe ${addr}` 
+    : `port ${port}` ;
+  debug('Listening on ' + bind);
+  //Desestrecuturando port de addr
+  console.log(`Listening at http://localhost:${port}`);
+
+}
+Footer
